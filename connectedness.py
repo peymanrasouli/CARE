a160b7c26706e30eeceec35c053419af23315930
peymanras <peyman.rasouli1@gmail.com>
2020-10-23 18:01:48 +0200

    updated

--- connectedness.py
+++ connectedness.py
@@ -2,7 +2,5 @@ import hdbscan
 
 def connectedness(cf_theta, connectedness_model):
     test_label, strength = hdbscan.approximate_predict(connectedness_model, cf_theta.reshape(1,-1))
-    # fitness = int(strength[0] > 0.5)
-    # fitness = int(strength[0] > 0.0)
-    fitness = strength[0]
+    fitness = int(strength[0] > 0.0)
     return fitness
