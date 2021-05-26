86c51705c6b632f5a616cf9ca61bbcd63317c3f2
peymanra <peymanra@ifi.uio.no>
2021-05-26 10:28:55 +0200

    updated

--- validations/care_coherency_preservation.py
+++ validations/care_coherency_preservation.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split
 from create_model import CreateModel, MLPClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from care.care import CARE
+from user_preferences import userPreferences
 from evaluate_counterfactuals import evaluateCounterfactuals
 
 def main():
@@ -70,21 +71,23 @@ def main():
             eval_results_csv = open(
                 experiment_path + 'care_coherency_preservation_%s_%s_eval_%s_%s.csv' % (dataset['name'], blackbox_name, N, n_cf), 'a')
 
-            header = ['Education','','',
-                      'Relationship', '', '']
+            header = ['Education','','', '',
+                      'Relationship', '', '', '']
             header = ','.join(header)
             eval_results_csv.write('%s\n' % (header))
             header = ['Validity',
                       'Validity+Soundness',
                       'Validity+Soundness+Coherency',
+                      'Validity+Soundness+Coherency+Actionability',
                       'Validity',
                       'Validity+Soundness',
-                      'Validity+Soundness+Coherency']
+                      'Validity+Soundness+Coherency',
+                      'Validity+Soundness+Coherency+Actionability']
             header = ','.join(header)
             eval_results_csv.write('%s\n' % (header))
-            average = '%s,%s,%s,%s,%s,%s\n' % \
-                     ('=average(A4:A1000)', '=average(B4:B1000)', '=average(C4:C1000)',
-                      '=average(D4:D1000)', '=average(E4:E1000)', '=average(F4:F1000)')
+            average = '%s,%s,%s,%s,%s,%s,%s,%s\n' % \
+                     ('=average(A4:A1000)', '=average(B4:B1000)', '=average(C4:C1000)', '=average(D4:D1000)',
+                      '=average(E4:E1000)', '=average(F4:F1000)', '=average(G4:G1000)', '=average(H4:H1000)')
             eval_results_csv.write(average)
             eval_results_csv.flush()
 
@@ -102,6 +105,11 @@ def main():
             care_config_123 = CARE(dataset, task=task, predict_fn=predict_fn, predict_proba_fn=predict_proba_fn,
                                     SOUNDNESS=True, COHERENCY=True, ACTIONABILITY=False, n_cf=n_cf)
             care_config_123.fit(X_train, Y_train)
+
+            # CARE with {validity, soundness, coherency, actionability} config
+            care_config_1234 = CARE(dataset, task=task, predict_fn=predict_fn, predict_proba_fn=predict_proba_fn,
+                                    SOUNDNESS=True, COHERENCY=True, ACTIONABILITY=True, n_cf=n_cf)
+            care_config_1234.fit(X_train, Y_train)
 
             # explaining instances from test set
             # correlation between education-num and education features
@@ -127,11 +135,13 @@ def main():
                 explanation_config_1 = care_config_1.explain(x_ord)
                 explanation_config_12 = care_config_12.explain(x_ord)
                 explanation_config_123 = care_config_123.explain(x_ord)
+                user_preferences = userPreferences(dataset, x_ord)
+                explanation_config_1234 = care_config_1234.explain(x_ord, user_preferences=user_preferences)
 
                 # evaluating counterfactuals based on all objectives results
-                toolbox = explanation_config_123['toolbox']
-                objective_names = explanation_config_123['objective_names']
-                featureScaler = explanation_config_123['featureScaler']
+                toolbox = explanation_config_1234['toolbox']
+                objective_names = explanation_config_1234['objective_names']
+                featureScaler = explanation_config_1234['featureScaler']
                 feature_names = dataset['feature_names']
 
                 # evaluating and recovering counterfactuals of {validity} config
@@ -225,6 +235,38 @@ def main():
                         relationship_preserved_config_123 += 1
                 relationship_preserved_config_123 = relationship_preserved_config_123 / n_cf
 
+                # evaluating and recovering counterfactuals of {validity, soundness, coherency, actionability} config
+                cfs_ord_config_1234, \
+                cfs_eval_config_1234, \
+                x_cfs_ord_config_1234, \
+                x_cfs_eval_config_1234 = evaluateCounterfactuals(x_ord, explanation_config_1234['cfs_ord'],
+                                                                 dataset, predict_fn, predict_proba_fn, task,
+                                                                 toolbox, objective_names, featureScaler,
+                                                                 feature_names)
+
+                # Education correlation set
+                education_num = cfs_ord_config_1234['education-num'].to_numpy().astype(int)
+                education = cfs_ord_config_1234['education'].to_numpy().astype(int)
+                education_preserved_config_1234 = 0
+                for n in range(n_cf):
+                    education_preserved_config_1234 += 1 if correlations[education_num[n]][1] == education[n] else 0
+                education_preserved_config_1234 = education_preserved_config_1234 / n_cf
+
+                # Relationship correlation set
+                relationship = cfs_ord_config_1234['relationship'].to_numpy().astype(int)
+                marital_status = cfs_ord_config_1234['marital-status'].to_numpy().astype(int)
+                sex = cfs_ord_config_1234['sex'].to_numpy().astype(int)
+                relationship_preserved_config_1234 = 0
+                for n in range(n_cf):
+                    if relationship[n] == 0:
+                        relationship_preserved_config_1234 += 1 if sex[n]==1 and marital_status[n]==2  else 0
+                    elif relationship[n] == 5:
+                        relationship_preserved_config_1234 += 1 if sex[n]==0 and marital_status[n]==2  else 0
+                    else:
+                        relationship_preserved_config_1234 += 1
+                relationship_preserved_config_1234 = relationship_preserved_config_1234 / n_cf
+
+
                 print('\n')
                 print('-------------------------------')
                 print("%s | %s: %d/%d explained" % (dataset['name'], blackbox_name, explained, N))
@@ -233,20 +275,25 @@ def main():
                 print(cfs_ord_config_1)
                 print(cfs_ord_config_12)
                 print(cfs_ord_config_123)
+                print(cfs_ord_config_1234)
 
                 print('\n')
                 print("Preserved Education    coherency | Validity: %0.3f - Validity+Soundness: %0.3f - "
-                      "Validity+Soundness+Coherency: %0.3f" %
-                      (education_preserved_config_1, education_preserved_config_12, education_preserved_config_123))
+                      "Validity+Soundness+Coherency: %0.3f - Validity+Soundness+Coherency+Actionability: %0.3f" %
+                      (education_preserved_config_1, education_preserved_config_12,
+                       education_preserved_config_123, education_preserved_config_1234))
                 print("Preserved Relationship coherency | Validity: %0.3f - Validity+Soundness: %0.3f - "
-                      "Validity+Soundness+Coherency: %0.3f" %
-                      (relationship_preserved_config_1, relationship_preserved_config_12, relationship_preserved_config_123))
-                print('----------------------------------------------------------'
-                      '----------------------------------------------------------')
+                      "Validity+Soundness+Coherency: %0.3f - Validity+Soundness+Coherency+Actionability: %0.3f" %
+                      (relationship_preserved_config_1, relationship_preserved_config_12,
+                       relationship_preserved_config_123, relationship_preserved_config_1234))
+                print('-------------------------------------------------------------------------------------'
+                      '-------------------------------------------------------------------------------------')
 
                 # storing the evaluation of the best counterfactual found by methods
-                eval_results = np.r_[education_preserved_config_1, education_preserved_config_12, education_preserved_config_123,
-                                     relationship_preserved_config_1, relationship_preserved_config_12, relationship_preserved_config_123]
+                eval_results = np.r_[education_preserved_config_1, education_preserved_config_12,
+                                     education_preserved_config_123, education_preserved_config_1234,
+                                     relationship_preserved_config_1, relationship_preserved_config_12,
+                                     relationship_preserved_config_123, relationship_preserved_config_1234]
                 eval_results = ['%.3f' % (eval_results[i]) for i in range(len(eval_results))]
                 eval_results = ','.join(eval_results)
                 eval_results_csv.write('%s\n' % (eval_results))
