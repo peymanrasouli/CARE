ed7b7d151f11fe5354a26e2318b054d3facca391
peymanrasouli <peyman.rasouli1@gmail.com>
2021-09-30 14:45:47 +0200

    updated

--- validations/care_coherency.py
+++ validations/care_coherency.py
@@ -36,11 +36,20 @@ def main():
         'gb-c': GradientBoostingClassifier
     }
 
+    # defining number of samples N and number of counterfactuals generated for every instance n_cf
     experiment_size = {
         'adult': (500, 10),
         'compas-scores-two-years': (500, 10),
         'credit-card-default': (500, 10),
         'heloc': (500,10),
+    }
+
+    # defining the features that are used for coherency evaluation
+    studied_features ={
+        'adult': [6, 7, 10, 11, 12],
+        'compas-scores-two-years': [4, 5, 7, 8],
+        'credit-card-default': [2, 3, 4, 5, 6, 7, 18, 19, 20, 21, 22],
+        'heloc': [0, 4, 5, 6, 15, 16],
     }
 
     for dataset_kw in datsets_list:
@@ -72,17 +81,17 @@ def main():
 
             # CARE with {validity} config
             care_config_1 = CARE(dataset, task=task, predict_fn=predict_fn, predict_proba_fn=predict_proba_fn,
-                                 SOUNDNESS=False, COHERENCY=False, ACTIONABILITY=False, n_cf=n_cf)
+                                 SOUNDNESS=False, COHERENCY=False, ACTIONABILITY=True, n_cf=n_cf)
             care_config_1.fit(X_train, Y_train)
 
             # CARE with {validity, soundness} config
             care_config_12 = CARE(dataset, task=task, predict_fn=predict_fn, predict_proba_fn=predict_proba_fn,
-                                  SOUNDNESS=True, COHERENCY=False, ACTIONABILITY=False, n_cf=n_cf)
+                                  SOUNDNESS=True, COHERENCY=False, ACTIONABILITY=True, n_cf=n_cf)
             care_config_12.fit(X_train, Y_train)
 
             # CARE with {validity, soundness, coherency} config
             care_config_123 = CARE(dataset, task=task, predict_fn=predict_fn, predict_proba_fn=predict_proba_fn,
-                                    SOUNDNESS=True, COHERENCY=True, ACTIONABILITY=False, n_cf=n_cf)
+                                    SOUNDNESS=True, COHERENCY=True, ACTIONABILITY=True, n_cf=n_cf)
             care_config_123.fit(X_train, Y_train)
 
             # CARE with {validity, soundness, coherency, actionability} config
@@ -90,92 +99,105 @@ def main():
                                     SOUNDNESS=True, COHERENCY=True, ACTIONABILITY=True, n_cf=n_cf)
             care_config_1234.fit(X_train, Y_train)
 
-            explained = 0
-            original_data = []
-            care_config_1_cfs = []
-            care_config_12_cfs = []
-            care_config_123_cfs = []
-            care_config_1234_cfs = []
-            for x_ord in X_test:
+            # generating counterfactuals in which the studied feature will change
+            for f in studied_features[dataset_kw]:
+                explained = 0
+                original_data = []
+                care_config_1_cfs = []
+                care_config_12_cfs = []
+                care_config_123_cfs = []
+                care_config_1234_cfs = []
+                feature_name = dataset['feature_indices'][f]
 
-                # explaining instances
-                explanation_config_1 = care_config_1.explain(x_ord)
-                explanation_config_12 = care_config_12.explain(x_ord)
-                explanation_config_123 = care_config_123.explain(x_ord)
-                user_preferences = userPreferences(dataset, x_ord)
-                explanation_config_1234 = care_config_1234.explain(x_ord, user_preferences=user_preferences)
+                feature_preference = {
+                    'constraint': [None for _ in range(X_train.shape[1])],
+                    'importance': [None for _ in range(X_train.shape[1])]
+                }
+                feature_preference['constraint'][f] = 'change'
+                feature_preference['importance'][f] = 10
 
-                # extracting the best counterfactual
-                care_config_1_best_cf = explanation_config_1['best_cf_ord']
-                care_config_12_best_cf = explanation_config_12['best_cf_ord']
-                care_config_123_best_cf = explanation_config_123['best_cf_ord']
-                care_config_1234_best_cf = explanation_config_1234['best_cf_ord']
+                user_preferences = userPreferences(dataset, X_train[0,:])
+                user_preferences['constraint'][f] = 'change'
+                user_preferences['importance'][f] = 10
 
-                original_data.append(x_ord)
-                care_config_1_cfs.append(care_config_1_best_cf)
-                care_config_12_cfs.append(care_config_12_best_cf)
-                care_config_123_cfs.append(care_config_123_best_cf)
-                care_config_1234_cfs.append(care_config_1234_best_cf)
+                for x_ord in X_test:
+                    # explaining instances
+                    explanation_config_1 = care_config_1.explain(x_ord, user_preferences=feature_preference)
+                    explanation_config_12 = care_config_12.explain(x_ord, user_preferences=feature_preference)
+                    explanation_config_123 = care_config_123.explain(x_ord, user_preferences=feature_preference)
+                    explanation_config_1234 = care_config_1234.explain(x_ord, user_preferences=user_preferences)
 
-                explained += 1
+                    # extracting the best counterfactual
+                    care_config_1_best_cf = explanation_config_1['best_cf_ord']
+                    care_config_12_best_cf = explanation_config_12['best_cf_ord']
+                    care_config_123_best_cf = explanation_config_123['best_cf_ord']
+                    care_config_1234_best_cf = explanation_config_1234['best_cf_ord']
 
-                print('-----------------------------------------------------------------------')
-                print("%s|%s: %d/%d explained" % (dataset['name'], blackbox_name, explained, N))
-                print('-----------------------------------------------------------------------')
+                    original_data.append(x_ord)
+                    care_config_1_cfs.append(care_config_1_best_cf)
+                    care_config_12_cfs.append(care_config_12_best_cf)
+                    care_config_123_cfs.append(care_config_123_best_cf)
+                    care_config_1234_cfs.append(care_config_1234_best_cf)
 
-                if explained == N:
-                    break
+                    explained += 1
 
-            # calculating the coherency presenvation rate
-            original_data = np.vstack(original_data)
-            cf_data = {
-                'care_config_1': np.vstack(care_config_1_cfs),
-                'care_config_12': np.vstack(care_config_12_cfs),
-                'care_config_123': np.vstack(care_config_123_cfs),
-                'care_config_1234': np.vstack(care_config_1234_cfs)
-            }
+                    print('----------------------------------------------------------------------------------------')
+                    print("%s|%s|%s: %d/%d explained" % (dataset['name'], blackbox_name, feature_name, explained, N))
+                    print('----------------------------------------------------------------------------------------')
 
-            if os.path.isfile(experiment_path + '%s_%s_original_correlation.csv' % (dataset_kw, blackbox_name)):
-                os.remove(experiment_path + '%s_%s_original_correlation.csv' % (dataset_kw, blackbox_name))
+                    if explained == N:
+                        break
 
-            if os.path.isfile(experiment_path + '%s_%s_counterfactual_correlation.csv' % (dataset_kw, blackbox_name)):
-                os.remove(experiment_path + '%s_%s_counterfactual_correlation.csv' % (dataset_kw, blackbox_name))
+                # calculating the coherency preservation rate
+                original_data = np.vstack(original_data)
+                cf_data = {
+                    'care_config_1': np.vstack(care_config_1_cfs),
+                    'care_config_12': np.vstack(care_config_12_cfs),
+                    'care_config_123': np.vstack(care_config_123_cfs),
+                    'care_config_1234': np.vstack(care_config_1234_cfs)
+                }
 
-            if os.path.isfile(experiment_path + '%s_%s_correlation_difference.csv' % (dataset_kw, blackbox_name)):
-                os.remove(experiment_path + '%s_%s_correlation_difference.csv' % (dataset_kw, blackbox_name))
+                if os.path.isfile(experiment_path + '%s_%s_original_correlation_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name)):
+                    os.remove(experiment_path + '%s_%s_original_correlation_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name))
 
-            original_data_df = pd.DataFrame(columns=dataset['feature_names'], data=original_data)
-            original_corr = nominal.associations(original_data_df, nominal_columns=dataset['discrete_features'])['corr']
-            original_corr = original_corr.round(decimals=3)
-            original_corr.to_csv(experiment_path + '%s_%s_original_correlation.csv' % (dataset_kw, blackbox_name))
+                if os.path.isfile(experiment_path + '%s_%s_counterfactual_correlation_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name)):
+                    os.remove(experiment_path + '%s_%s_counterfactual_correlation_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name))
 
-            for method, cfs in cf_data.items():
+                if os.path.isfile(experiment_path + '%s_%s_correlation_difference_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name)):
+                    os.remove(experiment_path + '%s_%s_correlation_difference_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name))
 
-                with open(experiment_path + '%s_%s_counterfactual_correlation.csv' % (dataset_kw, blackbox_name), 'a') as f:
-                    f.write(method)
-                    f.write('\n')
-                counterfactual_data = np.r_[original_data, cfs]
-                counterfactual_data_df = pd.DataFrame(columns=dataset['feature_names'], data=counterfactual_data)
-                counterfactual_corr = nominal.associations(counterfactual_data_df, nominal_columns=dataset['discrete_features'])['corr']
-                counterfactual_corr = counterfactual_corr.round(decimals=3)
-                counterfactual_corr.to_csv(experiment_path + '%s_%s_counterfactual_correlation.csv' % (dataset_kw, blackbox_name), mode='a')
-                with open(experiment_path + '%s_%s_counterfactual_correlation.csv' % (dataset_kw, blackbox_name), 'a') as f:
-                    f.write('\n')
+                original_data_df = pd.DataFrame(columns=dataset['feature_names'], data=original_data)
+                original_corr = nominal.associations(original_data_df, nominal_columns=dataset['discrete_features'])['corr']
+                original_corr = original_corr.round(decimals=3)
+                original_corr.to_csv(experiment_path + '%s_%s_original_correlation_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name))
 
-                with open(experiment_path + '%s_%s_correlation_difference.csv' % (dataset_kw, blackbox_name),'a') as f:
-                    f.write(method)
-                    f.write('\n')
-                correlation_diff = np.abs(original_corr - counterfactual_corr)
-                correlation_diff = correlation_diff.round(decimals=3)
-                correlation_diff.to_csv(experiment_path + '%s_%s_correlation_difference.csv' % (dataset_kw, blackbox_name), mode='a')
+                for method, cfs in cf_data.items():
 
-                with open(experiment_path + '%s_%s_correlation_difference.csv' % (dataset_kw, blackbox_name), 'a') as f:
-                    f.write('Feature-wise MAE:')
-                correlation_diff.mean().round(3).to_csv(experiment_path + '%s_%s_correlation_difference.csv' % (dataset_kw, blackbox_name), mode='a')
+                    with open(experiment_path + '%s_%s_counterfactual_correlation_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name), 'a') as f:
+                        f.write(method)
+                        f.write('\n')
+                    counterfactual_data = np.r_[original_data, cfs]
+                    counterfactual_data_df = pd.DataFrame(columns=dataset['feature_names'], data=counterfactual_data)
+                    counterfactual_corr = nominal.associations(counterfactual_data_df, nominal_columns=dataset['discrete_features'])['corr']
+                    counterfactual_corr = counterfactual_corr.round(decimals=3)
+                    counterfactual_corr.to_csv(experiment_path + '%s_%s_counterfactual_correlation_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name), mode='a')
+                    with open(experiment_path + '%s_%s_counterfactual_correlation_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name), 'a') as f:
+                        f.write('\n')
 
-                with open(experiment_path + '%s_%s_correlation_difference.csv' % (dataset_kw, blackbox_name), 'a') as f:
-                    f.write('Total MAE: ' + str(np.round(correlation_diff.mean().mean(), 3)))
-                    f.write('\n \n')
+                    with open(experiment_path + '%s_%s_correlation_difference_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name),'a') as f:
+                        f.write(method)
+                        f.write('\n')
+                    correlation_diff = np.abs(original_corr - counterfactual_corr)
+                    correlation_diff = correlation_diff.round(decimals=3)
+                    correlation_diff.to_csv(experiment_path + '%s_%s_correlation_difference_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name), mode='a')
+
+                    with open(experiment_path + '%s_%s_correlation_difference_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name), 'a') as f:
+                        f.write('Feature-wise MAE:')
+                    correlation_diff.mean().round(3).to_csv(experiment_path + '%s_%s_correlation_difference_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name), mode='a')
+
+                    with open(experiment_path + '%s_%s_correlation_difference_feature_%s.csv' % (dataset_kw, blackbox_name, feature_name), 'a') as f:
+                        f.write('Total MAE: ' + str(np.round(correlation_diff.mean().mean(), 3)))
+                        f.write('\n \n')
 
             print('Done!')
 
